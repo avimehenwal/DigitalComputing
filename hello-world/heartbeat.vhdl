121ccf24a10a8c6853ebd08815bfa2bc39aa0d0d
avimehenwal <avi.mehanwal@gmail.com>
2018-11-09 11:08:42 +0100

    some more files

---
+++ hello-world/heartbeat.vhdl
@@ -0,0 +1,15 @@
+entity hello_world is
+  port ( clk: out std_logic; )
+end hearbeat;
+
+architecture behaviour of hello_world is
+begin
+  -- Clock process definition
+  clk_process: process
+  begin
+    clk <= '0';
+    wait for clk_period/2;
+    clk <= '1';
+    wait for clk_period/2;
+  end process;
+end behaviour;
--- hello-world/hello.vhdl
+++ hello-world/hello.vhdl
@@ -1,15 +1,17 @@
-use std.textio.all;
+--  Hello world program
+use std.textio.all; -- Imports the standard textio package.
 
-entity hello-world is
-end hello-world;
+--  Defines a design entity, without any ports.
+entity hello_world is
+end hello_world;
 
-architecture behaviour of hello-world is
+architecture behaviour of hello_world is
 begin
-    process
-        variable l: line;
-    begin
-        write(l, String("Hello World"));
-        writeline (output, l);
-        wait;
-    end process;
+  process
+    variable l : line;
+  begin
+    write (l, String'("Hello world!"));
+    writeline (output, l);
+    wait;
+  end process;
 end behaviour;
