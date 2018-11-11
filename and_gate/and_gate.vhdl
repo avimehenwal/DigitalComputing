fea5c0cab3e3dc3efd0c8bc39abeb98fca7c5d2d
avimehenwal <avi.mehanwal@gmail.com>
2018-11-11 20:32:27 +0100

    test bench with configuration and assert, report seviarity block

---
+++ and_gate/and_gate.vhdl
@@ -0,0 +1,14 @@
+--import std_logic from the IEEE library
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity andGate is					
+   port( A, B : in std_logic;
+            F : out std_logic);
+end andGate;
+
+--FUNCTIONAL DESCRIPTION: how the AND Gate works
+architecture func of andGate is 
+begin
+  F <= A and B;		
+end func;
